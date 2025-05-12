9b4fac158a672a9834e7b5e181d34e3ef784deb5
guimaggiorini <guimaggiorini@gmail.com>
2025-05-12 20:12:11 -0300

    refact: update color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,21 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#3E62F1'
+const tintColorDark = '#4084FF'
 
 export default {
   light: {
     text: '#000',
     background: '#fff',
+    border: '#D4DEEA',
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#5A6577',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
     background: '#000',
+    border: '#1F2934',
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#96A2B5',
     tabIconSelected: tintColorDark,
   },
-};
+}
